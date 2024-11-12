f41948e0060e4e6891536bec3305f59d76465ba7
petebarbosa <pedrovbb89@gmail.com>
2024-11-12 15:18:45 -0300

    fix: projects import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Experiences from "@/components/Experiences";
-import Projects from "@/components/Contact";
+import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
