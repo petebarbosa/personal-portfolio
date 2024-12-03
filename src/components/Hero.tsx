c6923442713db40deb0c567d14914c16cd7b6795
petebarbosa <pedrovbb89@gmail.com>
2024-12-03 16:17:14 -0300

    chore: change avatar into an component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,40 @@
-import Image from "next/image";
-import avatar from "@/app/assets/avatar.png";
+"use client";
+
+import avatarImage from "@/app/assets/avatar.png";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { calculateExperience } from "@/utils/calculateExperience";
+import { Button } from "./ui/button";
+import { MapPinHouse } from "lucide-react";
+import { motion } from "motion/react";
 
 const Hero = () => {
   return (
-    <section id="home" className="pt-12 min-h-screen flex items-center">
+    <section id="home" className="pt-12 h-screen flex items-center">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div>
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+        <div className="flex items-center">
+          <Avatar>
+            <AvatarImage src={avatarImage.src} alt="A picture of Pedro Barbosa" />
+            <AvatarFallback>PB</AvatarFallback>
+          </Avatar>
+          <div className="pl-24">
+            <motion.h1
+              className="text-4xl md:text-6xl font-bold mb-2"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, duration: 1 }}
+            >
               Welcome!
-            </h1>
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">
-              Please, call me Pete!
-            </h1>
-            <p className="text-lg text-gray-600 mb-8">
-              I'm a software engineer with {calculateExperience()} of experience,
-              dedicated to createing efficient and innovative solutions.
-              I specialize in web development and love turning complex problems into
-              simple and beautiful applications.
+            </motion.h1>
+            <h2 className="text-4xl md:text-4xl font-bold mb-2">
+              Please, call me Pete.
+            </h2>
+            <p className="text-lg text-gray-600 mb-2">
+              I'm Pedro Barbosa, a software engineer with {calculateExperience()} of experience.
             </p>
-            <a href="#contact" className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors">
-              Get in touch
-            </a>
-          </div>
-          <div className="relative h-[400px] w-full">
-            <Image src={avatar} alt="Pedro's photo" className="rounded-lg object-cover xl:ml-20" />
+            <div className="flex space-x-2 mb-4">
+              <MapPinHouse size={22} strokeWidth={0.5} />
+              <p>Brazil, RJ</p>
+            </div>
+            <Button variant="outline" size="default">Get in touch</Button>
           </div>
         </div>
       </div>
