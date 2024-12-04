9cf339b8367cbbe005959fc83844890dca55bd22
petebarbosa <pedrovbb89@gmail.com>
2024-12-04 15:38:31 -0300

    feat: add contact links

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,37 +4,68 @@ import avatarImage from "@/app/assets/avatar.png";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { calculateExperience } from "@/utils/calculateExperience";
 import { Button } from "./ui/button";
-import { MapPinHouse } from "lucide-react";
+import { AtSign, MapPinHouse, Linkedin, Github, Send } from "lucide-react";
 import { motion } from "motion/react";
 
 const Hero = () => {
   return (
-    <section id="home" className="pt-12 h-screen flex items-center">
+    <section id="profile" className="pt-12 h-screen flex items-center">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
-          <Avatar>
-            <AvatarImage src={avatarImage.src} alt="A picture of Pedro Barbosa" />
-            <AvatarFallback>PB</AvatarFallback>
+          <Avatar className="ml-12">
+            <AvatarImage
+              src={avatarImage.src}
+              alt="A picture of Pedro Barbosa"
+            />
           </Avatar>
-          <div className="pl-24">
-            <motion.h1
-              className="text-4xl md:text-6xl font-bold mb-2"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, duration: 1 }}
-            >
+          <div className="pl-24 pr-24">
+            <h1 className="text-4xl md:text-6xl font-bold mb-2" >
               Welcome!
-            </motion.h1>
-            <h2 className="text-4xl md:text-4xl font-bold mb-2">
+            </h1>
+            <h2 className="text-4xl md:text-4xl font-bold mb-2" >
               Please, call me Pete.
             </h2>
-            <p className="text-lg text-gray-600 mb-2">
-              I'm Pedro Barbosa, a software engineer with {calculateExperience()} of experience.
+            <p className="text-lg text-gray-600 mb-2" >
+              I'm Pedro Barbosa, a full-stack developer with {calculateExperience()} of experience.
             </p>
             <div className="flex space-x-2 mb-4">
               <MapPinHouse size={22} strokeWidth={0.5} />
-              <p>Brazil, RJ</p>
+              <p className="text-gray-600">Brazil, RJ</p>
             </div>
-            <Button variant="outline" size="default">Get in touch</Button>
+            <div className="flex space-x-2">
+              <a
+                className="transition ease-in-out hover:scale-125 duration-300"
+                href="https://www.linkedin.com/in/pvbb250689/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Linkedin size={24} strokeWidth={0.75} />
+              </a>
+              <a
+                className="transition ease-in-out hover:scale-125 duration-300"
+                href="https://github.com/petebarbosa"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github size={24} strokeWidth={0.75} />
+              </a>
+              <a
+                className="transition ease-in-out hover:scale-125 duration-300"
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AtSign size={24} strokeWidth={0.75} />
+              </a>
+              <a
+                className="transition ease-in-out hover:scale-125 duration-300"
+                href="https://t.me/Pedrovbb"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Send size={24} strokeWidth={0.75} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -43,4 +74,3 @@ const Hero = () => {
 }
 
 export default Hero;
-
